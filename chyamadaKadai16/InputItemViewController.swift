877de5d42bf1376d00b654c5f21d5373d288eb4e
toaster <ko8au3s9b21@gmail.com>
2021-10-24 17:56:24 +0900

    first commit

---
+++ chyamadaKadai16/InputItemViewController.swift
@@ -0,0 +1,59 @@
+//
+//  AddItemViewController.swift
+//  chyamadaKadai13
+//
+//  Created by toaster on 2021/10/05.
+//
+
+import UIKit
+
+final class InputItemViewController: UIViewController {
+    enum Mode {
+        case add, edit(Fruit)
+    }
+    var mode: Mode?
+    
+    private(set) var fruit: Fruit?
+    private(set) var isChecked: Bool?
+    
+    @IBOutlet private weak var itemNameTextField: UITextField!
+    @IBOutlet private weak var saveBarButtonItem: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        saveBarButtonItem.isEnabled = false
+        itemNameTextField.addTarget(self,
+                                    action: #selector(itemNameTextFieldEditingChanged),
+                                    for: .editingChanged)
+        
+        switch mode {
+        case .add:
+            itemNameTextField.text = ""
+        case let .edit(fruit):
+            saveBarButtonItem.isEnabled = true
+            isChecked = fruit.isChecked
+            itemNameTextField.text = fruit.name
+        default:
+            break
+        }
+    }
+    
+    @IBAction private func saveItem(_ sender: Any) {
+        switch mode {
+        case .add:  performSegue(withIdentifier: "AddItemSegue", sender: nil)
+        case .edit: performSegue(withIdentifier: "EditItemSegue", sender: nil)
+        default:
+            break
+        }
+    }
+    
+    @objc private func itemNameTextFieldEditingChanged() {
+        saveBarButtonItem.isEnabled = isValid(itemName: itemNameTextField.text ?? "")
+        fruit = Fruit(name: itemNameTextField.text ?? "", isChecked: isChecked ?? false)
+    }
+    
+    private func isValid(itemName: String) -> Bool {
+        !itemName
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .isEmpty
+    }
+}
---
+++ chyamadaKadai16/ItemListViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  chyamadaKadai13
+//
+//  Created by 山田　天星 on 2021/09/28.
+//
+
+import UIKit
+
+struct Fruit {
+    var name: String
+    var isChecked: Bool
+}
+
+final class ItemListViewController: UIViewController {
+    private var fruitsList: [Fruit] = [Fruit(name: "りんご", isChecked: false),
+                                       Fruit(name: "みかん", isChecked: true),
+                                       Fruit(name: "バナナ", isChecked: false),
+                                       Fruit(name: "パイナップル", isChecked: true)
+    ]
+    
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.register(ItemListTableViewCell.nibName,
+                               forCellReuseIdentifier: ItemListTableViewCell.nibID)
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
+    private var editingAtIndexPath: IndexPath?
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let nextVC = (segue.destination as? UINavigationController)?.topViewController as? InputItemViewController {
+            switch segue.identifier ?? "" {
+            case "AddItemSegue":
+                nextVC.mode = .add
+            case "EditItemSegue":
+                guard let editingAtIndexPath = editingAtIndexPath else { return }
+                nextVC.mode = .edit(fruitsList[editingAtIndexPath.row])
+            default:
+                break
+            }
+        }
+    }
+}
+
+extension ItemListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { 40 }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        editingAtIndexPath = indexPath
+        performSegue(withIdentifier: "EditItemSegue", sender: nil)
+    }
+}
+
+extension ItemListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        fruitsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ItemListTableViewCell.nibID, for: indexPath)
+                as? ItemListTableViewCell else {
+            fatalError("The dequeued cell is not instance")
+        }
+        
+        let fruit = fruitsList[indexPath.row]
+        cell.configure(fruit: fruit)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        fruitsList[indexPath.row].isChecked.toggle()
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+    }
+}
+
+extension ItemListViewController {
+    @IBAction private func didTapCancelButton(segue: UIStoryboardSegue) { }
+    
+    @IBAction private func addItem(segue: UIStoryboardSegue) {
+        guard let sourceViewController = segue.source as? InputItemViewController,
+              let fruit = sourceViewController.fruit else {
+            return
+        }
+        
+        fruitsList.append(Fruit(name: fruit.name, isChecked: false))
+        tableView.reloadData()
+    }
+    
+    @IBAction private func editItem(segue: UIStoryboardSegue) {
+        guard let sourceViewController = segue.source as? InputItemViewController,
+              let fruit = sourceViewController.fruit,
+              let editingAtIndexPath = editingAtIndexPath else {
+            return
+        }
+        
+        fruitsList[editingAtIndexPath.row] = fruit
+        tableView.reloadRows(at: [editingAtIndexPath], with: .automatic)
+    }
+}
